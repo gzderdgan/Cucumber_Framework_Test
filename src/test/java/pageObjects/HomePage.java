c1c95a70de1fb57a6720945bb1b9fbe0ec50f0df
eyeliner <eyeliner@gmail.com>
2023-02-22 11:18:27 +0000

    commit msg

---
+++ src/test/java/pageObjects/HomePage.java
@@ -0,0 +1,55 @@
+package pageObjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class HomePage extends BasePage{
+	
+	public HomePage(WebDriver driver) {
+		super(driver);//can used to invoke parent class constructor
+	}
+	
+	//Elements
+	@FindBy(xpath ="//a[@title='My Account']")
+	WebElement lnkMyaccount;
+	
+	@FindBy(linkText ="Register")
+	WebElement lnkRegister;
+	
+	@FindBy(linkText ="Login")
+	WebElement lnkLogin;
+	
+	@FindBy(xpath="//input[@placeholder='Search']")  //For Search Product Test
+	WebElement txtSearchbox;
+	
+	@FindBy(xpath="//div[@id='search']//button[@type='button']") //For Search Product Test
+	WebElement btnSearch;
+	
+	
+	
+	
+	//Action Methods
+	public void clickMyAccount() {
+		lnkMyaccount.click();
+	}
+	
+	public void clickRegister() {
+		lnkRegister.click();
+	}
+	
+	public void clickLogin() {
+		lnkLogin.click();
+	}
+	
+	public void enterProductName(String pName)   //For Search Product Test
+	{
+		txtSearchbox.sendKeys(pName);
+	}
+	
+	public void clickSearch()  //For Search Product Test
+	{
+		btnSearch.click();
+	}
+	
+}
